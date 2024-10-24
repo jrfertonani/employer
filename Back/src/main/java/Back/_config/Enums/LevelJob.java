6b309b85110ffe2fff3def8d1a088d33dada7452
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-24 18:30:18 -0300

    LevelJob  Enum

--- Back/src/main/java/Back/_config/Enums/LevelJob.java
+++ Back/src/main/java/Back/_config/Enums/LevelJob.java
@@ -3,7 +3,7 @@ package Back._config.Enums;
 import lombok.Getter;
 
 @Getter
-public enum levelWorking {
+public enum LevelJob {
     // aqui são os enums
     JUNIOR(0,"JUNIOR"),
     PLENO(1,"PLENO"),
@@ -14,23 +14,23 @@ public enum levelWorking {
     private final String descricao;
 
     //aqui construtor
-    levelWorking(Integer codigo, String descricao) {
+    LevelJob(Integer codigo, String descricao) {
         this.codigo = codigo;
         this.descricao = descricao;
     }
 
     //aqui um codigo para verificar se existe ou não de enum
     //se existir retorna ele se não retorna uma exceção (não necessario)
-    public static levelWorking toEnum(Integer cod){
+    public static LevelJob toEnum(Integer cod){
         if( cod == null){
             return null;
         }
-        for(levelWorking x : levelWorking.values()){
+        for(LevelJob x : LevelJob.values()){
             if(cod.equals(x.getCodigo())){
                 return x;
             }
         }
-        throw new IllegalArgumentException("Level working Invalid : " + cod);
+        throw new IllegalArgumentException("Level job Invalid : " + cod);
     }
 
 }
