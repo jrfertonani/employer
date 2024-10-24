af574e066cef7f9badcda1a90894f1cf51e6db38
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-24 18:45:22 -0300

    employer repository

--- Back/src/main/java/Back/employer/repository/employerRepository.java
+++ Back/src/main/java/Back/employer/repository/employerRepository.java
@@ -1,8 +1,9 @@
 package Back.employer.repository;
 
+import Back.employer.domain.entity.Employer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface employerRepository extends JpaRepository<> {
+public interface employerRepository extends JpaRepository<Employer, Long> {
 }
