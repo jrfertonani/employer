7d22f45c10bcb9416ce437be63b524eadb70ee85
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-24 18:45:36 -0300

    employer

--- Back/src/main/java/Back/employer/domain/entity/Employer.java
+++ Back/src/main/java/Back/employer/domain/entity/Employer.java
@@ -11,7 +11,7 @@ import java.io.Serial;
 import java.io.Serializable;
 
 @Data       @Entity
-public class employer implements Serializable {
+public class Employer implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
