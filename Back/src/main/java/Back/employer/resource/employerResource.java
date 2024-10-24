bdeeeee17499e2b78330da8b441ec3886240706b
Ademir Fertonani <ademirjuniorfertonani@gmail.com>
2024-10-24 17:47:27 -0300

    employer resource

---
+++ Back/src/main/java/Back/employer/resource/employerResource.java
@@ -0,0 +1,23 @@
+package Back.employer.resource;
+
+
+import Back.employer.service.employerService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/employers")
+public class employerResource {
+
+    @Autowired
+    public ModelMapper mapper;
+
+    @Autowired
+    public employerService service;
+
+
+}
